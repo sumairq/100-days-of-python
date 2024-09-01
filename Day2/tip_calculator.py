b1a75f739e19079bbf16d96e86d752f23caa24fc
sumairq <sumairqaisar992@gmail.com>
2024-09-01 23:19:19 +0500

    Refactor tip calculator

--- Day2/tip_calculator.py
+++ Day2/tip_calculator.py
@@ -1,14 +1,13 @@
 print("Welcome to the tip Calculator!")
-bill = input("What was the total bill? $")
-tip = input("How much tip would you like to give? 10, 12, or 15?")
-num_people = input("How many people to split the bill?")
+bill = float(input("What was the total bill? $"))
+tip = int(input("How much tip would you like to give? 10, 12, or 15?"))
+num_people = int(input("How many people to split the bill?"))
 
-bill_int = int(bill)
-tip_int = int(tip)
 
-total_tip = (tip_int/100) * bill_int
-total_bill = bill_int + total_tip
-amount_to_each = total_bill / int(num_people) 
 
-print(f"Each person should pay: ${round(amount_to_each, 2)}")
+total_tip = (tip/100) * bill
+total_bill = bill + total_tip
+amount_per_person = round(total_bill / int(num_people), 2)
+
+print(f"Each person should pay: ${amount_per_person}")
       
